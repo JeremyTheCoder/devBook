c6f84f83c7caf820a08b14cc4d89eee166bdb16c
jeremy <jeremy@desktop.fios-router.home>
2019-07-07 18:37:04 -0400

    hang up on file path

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,42 @@
 const express = require('express');
 const router = express();
+const { check, validationResult } = require('express-validator');
 
-//@route   Get api/users
-//@desc    Test route
+const User = require('../../models/User');
+
+//@route   POST api/users
+//@desc    Register user
 //@access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    //See if user exists
+
+    //get users gravatar
+
+    //encrypt password
+
+    //return jsonwebtoken
+
+    res.send('User route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const { check, validationResult } = require('express-validator');
 
 const app = express();
 
 //Connect database
 connectDB();
 
+//Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
 
 //Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profiles', require('./routes/api/profile'));
+app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
