0b3b02fafa1af74730a3b7b37bc4b144f894e268
jeremy <jeremy@desktop.fios-router.home>
2019-08-17 00:28:46 -0400

    Prepared for deployment 2

--- server.js
+++ server.js
@@ -4,28 +4,28 @@ const path = require('path');
 
 const app = express();
 
-//Connect database
+// Connect Database
 connectDB();
 
-//Init middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-//Define routes
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-//Serve static assets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  //Set static folder
+  // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server starter on  ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
