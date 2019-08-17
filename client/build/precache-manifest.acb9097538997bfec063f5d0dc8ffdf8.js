84f7957596dba90b7296ce03e59f4123fda3f22e
jeremy <jeremy@desktop.fios-router.home>
2019-08-17 00:10:47 -0400

    npm build

---
+++ client/build/precache-manifest.acb9097538997bfec063f5d0dc8ffdf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54807ab866eda7f0778f968f6d73987c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "990fdfce95d3f87634a9",
+    "url": "/static/css/main.b2abb4d8.chunk.css"
+  },
+  {
+    "revision": "f2c735139f0718334ceb",
+    "url": "/static/js/2.be61a312.chunk.js"
+  },
+  {
+    "revision": "990fdfce95d3f87634a9",
+    "url": "/static/js/main.0a9f70e2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "5fe4039b11fa6d042519dd1d049901c9",
+    "url": "/static/media/spinner.5fe4039b.gif"
+  }
+]);
